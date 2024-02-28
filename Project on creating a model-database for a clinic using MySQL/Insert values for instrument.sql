434bd1f0fb55864540ca0f1eba79d0cf0c2cbd11
tanzir109 <63890322+tanzir109@users.noreply.github.com>
2024-02-28 19:01:23 +1100

    Add files via upload

---
+++ Project on creating a model-database for a clinic using MySQL/Insert values for instrument.sql
@@ -0,0 +1,63 @@
+insert into instrument
+values (1, 'Otoscope', 'Examines the ears', 700),
+(2, 'Pulse Oximeter', 'Monitor oxygen level', 300),
+(3, 'Ophthalmoscope', 'Examines the eyes and retina', 2650),
+(4, 'Tongue Depressor', 'Holds down the tongue during throat examinations', 800),
+(5, 'Reflex Hammer', 'Tests reflexes in the nervous system', 50),
+(6, 'Scalpel', 'A surgical knife used for incisions', 100),
+(7, 'Surgical Scissors', 'Used for cutting tissues during surgery', 60),
+(8, 'Forceps', 'Grasping and holding tissues or objects during surgery', 70),
+(9, 'Surgical Sutures', 'Used for stitching wounds', 50),
+(10, 'Hemostats', 'Controls bleeding by clamping blood vessels', 150),
+(11, 'Electrocardiogram (ECG)', 'Records electrical activity of the heart', 4500),
+(12, 'Ultrasound Machine', 'Uses sound waves for imaging internal organs', 19800),
+(13, 'X-ray Machine', 'Produces X-ray images for diagnostic purposes', 18000),
+(14, 'MRI Machine', 'Magnetic Resonance Imaging for detailed internal images', 60800),
+(15, 'CT Scanner', 'Computed Tomography scanner for detailed cross-sectional images', 47000),
+(16, 'Endoscope', 'A flexible tube with a camera for internal examinations', 7400),
+(17, 'Laryngoscope', 'Examines the throat and vocal cords', 1250),
+(18, 'Defibrillator', 'Restores normal heart rhythm during cardiac arrest', 700),
+(19, 'Infusion Pump', 'Delivers medications and fluids intravenously', 650),
+(20, 'Doppler Ultrasound', 'Measures blood flow in arteries and veins', 4500),
+(21, 'Glucometer', 'Measures blood glucose levels', 250),
+(22, 'Nebulizer', 'Converts liquid medication into a mist for inhalation', 260),
+(23, 'Spirometer', 'Measures lung functiony', 2450),
+(24, 'Fetal Doppler', 'Monitors the fetal heartbeat during pregnancy', 1500),
+(25, 'Blood Gas Analyzer', 'Measures blood gases and pH levels', 1250),
+(26, 'Hemodialysis Machine', 'Used for kidney dialysis', 15060),
+(27, 'C-arm Fluoroscope', 'Provides real-time X-ray imaging during surgery', 8850),
+(28, 'Telemetry Monitor', 'Continuously monitors vital signs remotely', 1400),
+(29, 'Microscopes', 'Used for analyzing blood and tissue samplesr', 4250),
+(30, 'Laryngoscope', 'Helps visualize the airway during intubation', 5620),
+(31, 'PCR Machine', 'Amplifies DNA for genetic testing and research', 4350),
+(32, 'HPLC', 'Separates and analyzes compounds in liquid samples', 20000),
+(33, 'Immunoassay Analyzer', 'Detects and quantifies specific molecules, such as hormones or antibodies, in blood or other fluids', 6000),
+(35, 'GC-MS', 'Separates and identifies volatile compounds in gas samples', 13000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
