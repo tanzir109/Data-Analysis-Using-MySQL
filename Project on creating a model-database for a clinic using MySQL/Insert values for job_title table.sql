0460987f852cbee35a41d5cc879ec39fa5b7dd44
tanzir109 <63890322+tanzir109@users.noreply.github.com>
2024-02-28 19:32:30 +1100

    Add files via upload

---
+++ Project on creating a model-database for a clinic using MySQL/Insert values for job_title table.sql
@@ -0,0 +1,11 @@
+insert into job_title
+values(1, 'Professor', 150000),
+(2, 'Associate Professor', 130000),
+(3, 'Assistant Professor', 110000),
+(4, 'Lecturer', 80000),
+(5, 'Senior Nurse', 90000),
+(6, 'Nurse', 70000)
+
+
+
+
