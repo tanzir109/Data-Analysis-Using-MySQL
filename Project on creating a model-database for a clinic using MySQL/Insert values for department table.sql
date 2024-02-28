77a70bdeb3bbce115e5d3db275a2febd124c9522
tanzir109 <63890322+tanzir109@users.noreply.github.com>
2024-02-28 19:18:08 +1100

    Add files via upload

---
+++ Project on creating a model-database for a clinic using MySQL/Insert values for department table.sql
@@ -0,0 +1,16 @@
+Insert into department
+values(1, 'Internal Medicine'),
+(2, 'Emergency'),
+(3, 'Surgical Ward'),
+(4, 'Intensive Care Unit'),
+(5, 'Pediatrics'),
+(6, 'Gynecology'),
+(7, 'Dermatology'),
+(8, 'Neurology'),
+(9, 'Psychiatry'),
+(10, 'Physical Therapy'),
+(11, 'Radiology')
+
+
+
+
