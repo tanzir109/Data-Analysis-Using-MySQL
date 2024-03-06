467c75354e2251cf7ca772a853423424d17bf88d
tanzir109 <63890322+tanzir109@users.noreply.github.com>
2024-03-06 14:31:17 +1100

    Add files via upload

---
+++ Project: Online Store/Insert values for shipping_method table.sql
@@ -0,0 +1,7 @@
+Insert into shipping_method
+value(1, "Australia Post_Reguar Mail < 500 g", 10),
+(2, "Australia Post_Regular Mail < 2 kg", 20),
+(3, "Australia Post_Regular Mail < 5 kg", 30),
+(4, "Australia Post_Express Mail < 500 g", 15),
+(5, "Australia Post_Express Mail < 2 kg", 25),
+(6, "Australia Post_Express Mail < 5 kg", 35)
