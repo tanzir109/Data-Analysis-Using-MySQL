7c06c76d6a904fdb5bb1f1ee8cdef7876a0b1985
tanzir109 <63890322+tanzir109@users.noreply.github.com>
2024-03-25 17:35:26 +1100

    Add files via upload

---
+++ Project: Online Store/Insert values for staff table.sql
@@ -0,0 +1,3 @@
+insert into staff
+values(1, "Julia", "Gillard", "julia.gillard@gmail.com", 0480365982, "15 New South Head Road, Double Bay, NSW", 1),
+(2, "Robert", "Elliot", "robert.elliot@hotmail.com", 0470256384, "2 Pellicon St, Bondi, NSW", 2)
